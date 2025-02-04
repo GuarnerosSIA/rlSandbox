4d73871d5ed395fedf63b947952bb79177ccced5
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-02-04 14:03:56 -0600

    Red importada de matlab a pytorch

---
+++ importingNN.py
@@ -0,0 +1,21 @@
+import torch
+import onnx2torch
+import onnx
+
+path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/rlSandbox/rlSandbox/actorRLSGP.onnx"
+model_actor = onnx.load(path)
+actor = onnx2torch.convert(model_actor)
+x = torch.ones((1,10))
+y = actor(x)
+print(y)
+
+
+path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/rlSandbox/rlSandbox/criticRLSGP1.onnx"
+model_critic = onnx.load(path)
+critic = onnx2torch.convert(model_critic)
+
+input1 = torch.ones((1,10))
+input2 = torch.ones((1,10))
+
+y = critic(input1,input2)
+print(y)
