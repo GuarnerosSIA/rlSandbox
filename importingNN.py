8a2feec57cf62e4e938cfca1417eb5efa7ddb5a1
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-02-05 17:58:12 -0600

    networks loaded

--- importingNN.py
+++ importingNN.py
@@ -2,20 +2,20 @@ import torch
 import onnx2torch
 import onnx
 
-path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/rlSandbox/rlSandbox/actorRLSGP.onnx"
+path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/ReinforcementLearningTD3/actorRLcart.onnx"
 model_actor = onnx.load(path)
 actor = onnx2torch.convert(model_actor)
-x = torch.ones((1,10))
+x = torch.zeros((1,4))
 y = actor(x)
 print(y)
 
 
-path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/rlSandbox/rlSandbox/criticRLSGP1.onnx"
+path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/ReinforcementLearningTD3/criticRLcart.onnx"
 model_critic = onnx.load(path)
 critic = onnx2torch.convert(model_critic)
 
-input1 = torch.zeros((1,10))
-input2 = torch.ones((1,10))
+input1 = torch.ones((1,4))
+input2 = torch.zeros((1,1))
 
 y = critic(input1,input2)
 print(y)
