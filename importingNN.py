a6db4f3d0b8aab45be0bf605c5e6fb0ecbdbcd32
Alejandro <alejandrowarnsan@gmail.com>
2025-02-06 10:44:33 -0600

    Modify some functions in operation and model from onnx2pytorch including the mumpy.copy

--- importingNN.py
+++ importingNN.py
@@ -1,13 +1,23 @@
 import torch
 import onnx2torch
+from onnx2pytorch import ConvertModel
 import onnx
 
-def importMatlabNN():
+def importMatlabNN(mode='ConvertModel'):
     path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/ReinforcementLearningTD3/actorRLcart.onnx"
     model_actor = onnx.load(path)
-    actor = onnx2torch.convert(model_actor)
+    if mode == 'ConvertModel':
+        actor = ConvertModel(model_actor)
+    else:
+        actor = onnx2torch.convert(model_actor)
 
     path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/ReinforcementLearningTD3/criticRLcart.onnx"
     model_critic = onnx.load(path)
-    critic = onnx2torch.convert(model_critic)
+    if mode == 'ConvertModel':
+        critic = ConvertModel(model_critic)
+    else:
+        critic = onnx2torch.convert(model_critic)
     return actor, critic
+
+if __name__ == "__main__":
+    importMatlabNN()
