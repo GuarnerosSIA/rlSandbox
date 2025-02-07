ee110ebf22e9a30417cf6c735bd5ad6245f5cef1
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-02-07 17:15:36 -0600

    Custom neural network

--- importingNN.py
+++ importingNN.py
@@ -1,34 +1,59 @@
 import torch
-import onnx2torch
-from onnx2pytorch import ConvertModel
-import onnx
+import scipy.io
+import numpy as np
+import torch.nn as nn
 
-class newConvertModel(ConvertModel):
-    def __init__(self,*args, **kwargs):
-        super(newConvertModel, self).__init__(*args, **kwargs)
-    
-    def set_training_mode(self,mode: bool)->None:
-        self.train(mode)
+data = scipy.io.loadmat("actorParams.mat")
+
+fc1_W = data["fc_1_W"]
+fc1_b = data["fc_1_b"]
+
+fc2_W = data["fc_2_W"]
+fc2_b = data["fc_2_b"]
+
+fc3_W = data["fc_3_W"]
+fc3_b = data["fc_3_b"]
+
+scaleW = data["scaleW"]
+
+class myModel(nn.Module):
+    def __init__(self):
+        super(myModel, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(4,128)
+        self.act1 = nn.ReLU()
+        self.fc2 = nn.Linear(128,128)
+        self.act2 = nn.ReLU()
+        self.fc3 = nn.Linear(128,1)
+        self.output = nn.Tanh()
+    def forward(self, x):
+        x = self.flatten(x)
+        out1 = self.fc1(x)
+        act1 = self.act1(out1)
+        out2 = self.fc2(act1)
+        act2 = self.act2(out2)
+        out3 = self.fc3(act2)
+        logits = self.output(out3)
+        return logits
 
 
-def importMatlabNN(mode='ConvertModel'):
-    path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/ReinforcementLearningTD3/actorRLcart.onnx"
-    model_actor = onnx.load(path)
-    if mode == 'ConvertModel':
-        actor = newConvertModel(model_actor)
-    else:
-        actor = onnx2torch.convert(model_actor)
+fc1_W_tensor = torch.from_numpy(fc1_W).float()
+fc1_b_tensor = torch.from_numpy(fc1_b[:,0]).float()
 
-    path = "C:/Users/guarn/Dropbox/Alejandro/DoctoradoITESM/ReinforcementLearningTD3/criticRLcart.onnx"
-    model_critic = onnx.load(path)
-    if mode == 'ConvertModel':
-        critic = newConvertModel(model_critic)
-    else:
-        critic = onnx2torch.convert(model_critic)
-    return actor, critic
+fc2_W_tensor = torch.from_numpy(fc2_W).float()
+fc2_b_tensor = torch.from_numpy(fc2_b[:,0]).float()
 
-if __name__ == "__main__":
-    actor, critic = importMatlabNN(mode='ConvertModel')
+fc3_W_tensor = torch.from_numpy(fc3_W).float()
+fc3_b_tensor = torch.from_numpy(fc3_b[:,0]).float()
 
-    
-    optimizer = torch.optim.Adam(actor.parameters())
+
+model = myModel()
+with torch.no_grad():
+    model.fc1.weight.copy_(fc1_W_tensor)
+    model.fc1.bias.copy_(fc1_b_tensor)
+    model.fc2.weight.copy_(fc2_W_tensor)
+    model.fc2.bias.copy_(fc2_b_tensor)
+    model.fc3.weight.copy_(fc3_W_tensor)
+    model.fc3.bias.copy_(fc3_b_tensor)
+
+
