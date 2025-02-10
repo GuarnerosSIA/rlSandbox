1f5f7e3409d12dbf1e795502163b2151b4de7442
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-02-10 15:31:47 -0600

    I now working with custom neural networks. Customize feature extractor and pass weights

--- customNeuralNetworks.py
+++ customNeuralNetworks.py
@@ -6,83 +6,103 @@ from torch import nn
 import gymnasium as gym
 
 from stable_baselines3 import TD3,PPO
-from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.policies import ActorCriticPolicy, BaseModel
+from stable_baselines3.td3.policies import Actor, TD3Policy
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.preprocessing import get_action_dim
 
 
-class CustomNetwork(nn.Module):
+class CustomActor(Actor):
     """
-    Custom network for policy and value function.
-    It receives as input the features extracted by the features extractor.
-
-    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
-    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
-    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    Actor network (policy) for TD3.
     """
+    def __init__(self, *args, **kwargs):
+        super(CustomActor, self).__init__(*args, **kwargs)
+        # Define custom network with Dropout
+        # WARNING: it must end with a tanh activation to squash the output
+        self.mu = nn.Sequential(nn.Flatten(),
+                                nn.Linear(4,128),
+                                nn.ReLU(),
+                                nn.Linear(128,128),
+                                nn.ReLU(),
+                                nn.Linear(128,1)
+                                )
 
+
+class CustomContinuousCritic(BaseModel):
+    """
+    Critic network(s) for DDPG/SAC/TD3.
+    """
     def __init__(
         self,
-        feature_dim: int,
-        last_layer_dim_pi: int = 64,
-        last_layer_dim_vf: int = 64,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        net_arch: List[int],
+        features_extractor: nn.Module,
+        features_dim: int,
+        activation_fn: Type[nn.Module] = nn.ReLU,
+        normalize_images: bool = True,
+        n_critics: int = 2,
+        share_features_extractor: bool = True,
     ):
-        super().__init__()
-
-        # IMPORTANT:
-        # Save output dimensions, used to create the distributions
-        self.latent_dim_pi = last_layer_dim_pi
-        self.latent_dim_vf = last_layer_dim_vf
-
-        # Policy network
-        self.policy_net = nn.Sequential(
-            nn.Linear(feature_dim,128),
-            nn.ReLU(),
-            nn.Linear(128,last_layer_dim_pi),
-            nn.ReLU()
-        )
-        # Value network
-        self.value_net = nn.Sequential(
-            nn.Linear(feature_dim, last_layer_dim_vf), nn.ReLU()
-        )
-
-    def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
-        """
-        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
-            If all layers are shared, then ``latent_policy == latent_value``
-        """
-        return self.forward_actor(features), self.forward_critic(features)
-
-    def forward_actor(self, features: th.Tensor) -> th.Tensor:
-        return self.policy_net(features)
-
-    def forward_critic(self, features: th.Tensor) -> th.Tensor:
-        return self.value_net(features)
-
-
-class CustomActorCriticPolicy(ActorCriticPolicy):
-    def __init__(
-        self,
-        observation_space: spaces.Space,
-        action_space: spaces.Space,
-        lr_schedule: Callable[[float], float],
-        *args,
-        **kwargs,
-    ):
-        # Disable orthogonal initialization
-        kwargs["ortho_init"] = False
         super().__init__(
             observation_space,
             action_space,
-            lr_schedule,
-            # Pass remaining arguments to base class
-            *args,
-            **kwargs,
+            features_extractor=features_extractor,
+            normalize_images=normalize_images,
         )
 
+        action_dim = get_action_dim(self.action_space)
 
-    def _build_mlp_extractor(self) -> None:
-        self.mlp_extractor = CustomNetwork(self.features_dim)
+        self.share_features_extractor = share_features_extractor
+        self.n_critics = n_critics
+        self.q_networks = []
+        for idx in range(n_critics):
+            # q_net = create_mlp(features_dim + action_dim, 1, net_arch, activation_fn)
+            # Define critic with Dropout here
+            q_net = nn.Sequential(nn.Flatten(),
+                                nn.Linear(5,128),
+                                nn.ReLU(),
+                                nn.Linear(128,128),
+                                nn.ReLU(),
+                                nn.Linear(128,1),
+                                nn.Tanh()
+                                )
+            self.add_module(f"qf{idx}", q_net)
+            self.q_networks.append(q_net)
+
+    def forward(self, obs: th.Tensor, actions: th.Tensor) -> Tuple[th.Tensor, ...]:
+        # Learn the features extractor using the policy loss only
+        # when the features_extractor is shared with the actor
+        with th.set_grad_enabled(not self.share_features_extractor):
+            features = self.extract_features(obs,self.features_extractor)
+        qvalue_input = th.cat([features, actions], dim=1)
+        return tuple(q_net(qvalue_input) for q_net in self.q_networks)
+
+    def q1_forward(self, obs: th.Tensor, actions: th.Tensor) -> th.Tensor:
+        """
+        Only predict the Q-value using the first network.
+        This allows to reduce computation when all the estimates are not needed
+        (e.g. when updating the policy in TD3).
+        """
+        with th.no_grad():
+            features = self.extract_features(obs,self.features_extractor)
+        return self.q_networks[0](th.cat([features, actions], dim=1))
+
+class CustomTD3Policy(TD3Policy):
+    def __init__(self, *args, **kwargs):
+        super(CustomTD3Policy, self).__init__(*args, **kwargs)
+
+    def make_actor(self, features_extractor: Optional[BaseFeaturesExtractor] = None) -> CustomActor:
+        actor_kwargs = self._update_features_extractor(self.actor_kwargs, features_extractor)
+        return CustomActor(**actor_kwargs).to(self.device)
+
+    def make_critic(self, features_extractor: Optional[BaseFeaturesExtractor] = None) -> CustomContinuousCritic:
+        critic_kwargs = self._update_features_extractor(self.critic_kwargs, features_extractor)
+        return CustomContinuousCritic(**critic_kwargs).to(self.device)
+
 
 env = gym.make('InvertedPendulum-v4',render_mode = "human")
-model = PPO(CustomActorCriticPolicy, env, verbose=1)
+model = TD3(CustomTD3Policy, env, verbose=1)
 print(model.policy)
 model.learn(1000)
