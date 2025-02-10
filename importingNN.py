8b9b638a7461e0d2b6360987ab5c63dd26e9f533
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-02-10 09:35:22 -0600

    actor is working

--- importingNN.py
+++ importingNN.py
@@ -16,9 +16,9 @@ fc3_b = data["fc_3_b"]
 
 scaleW = data["scaleW"]
 
-class myModel(nn.Module):
+class customActor(nn.Module):
     def __init__(self):
-        super(myModel, self).__init__()
+        super(customActor, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(4,128)
         self.act1 = nn.ReLU()
@@ -34,7 +34,7 @@ class myModel(nn.Module):
         act2 = self.act2(out2)
         out3 = self.fc3(act2)
         logits = self.output(out3)
-        return logits
+        return logits*10
 
 
 fc1_W_tensor = torch.from_numpy(fc1_W).float()
@@ -47,7 +47,7 @@ fc3_W_tensor = torch.from_numpy(fc3_W).float()
 fc3_b_tensor = torch.from_numpy(fc3_b[:,0]).float()
 
 
-model = myModel()
+model = customActor()
 with torch.no_grad():
     model.fc1.weight.copy_(fc1_W_tensor)
     model.fc1.bias.copy_(fc1_b_tensor)
@@ -55,5 +55,73 @@ with torch.no_grad():
     model.fc2.bias.copy_(fc2_b_tensor)
     model.fc3.weight.copy_(fc3_W_tensor)
     model.fc3.bias.copy_(fc3_b_tensor)
+
+X = torch.ones(1,4)
+
+logits = model(X)
+print(logits)
+
+#####
+
+# data = scipy.io.loadmat("criticParams1.mat")
+
+# fc1_W = data["fc1_W"]
+# fc1_b = data["fc1_b"]
+
+# fc2_W = data["fc2_W"]
+# fc2_b = data["fc2_b"]
+
+# fcbody_W = data["fcBody_W"]
+# fcbody_b = data["fcBody_b"]
+
+# fcoutput_W = data["fcoutput_W"]
+# fcoutput_b = data["fcoutput_b"]
+
+# class customCritic(nn.Module):
+#     def __init__(self):
+#         super(customCritic, self).__init__()
+#         self.flatten = nn.Flatten()
+#         self.fcin1 = nn.Linear(4,128)
+#         self.fcin2 = nn.Linear(1,128)
+        
+
+#         self.fc2 = nn.Linear(128,128)
+#         self.act2 = nn.ReLU()
+#         self.fc3 = nn.Linear(128,1)
+#         self.output = nn.Tanh()
+#     def forward(self, x):
+#         x = self.flatten(x)
+#         out1 = self.fc1(x)
+#         act1 = self.act1(out1)
+#         out2 = self.fc2(act1)
+#         act2 = self.act2(out2)
+#         out3 = self.fc3(act2)
+#         logits = self.output(out3)
+#         return logits*10
+
+
+# fc1_W_tensor = torch.from_numpy(fc1_W).float()
+# fc1_b_tensor = torch.from_numpy(fc1_b[:,0]).float()
+
+# fc2_W_tensor = torch.from_numpy(fc2_W).float()
+# fc2_b_tensor = torch.from_numpy(fc2_b[:,0]).float()
+
+# fc3_W_tensor = torch.from_numpy(fc3_W).float()
+# fc3_b_tensor = torch.from_numpy(fc3_b[:,0]).float()
+
+
+# model = customActor()
+# with torch.no_grad():
+#     model.fc1.weight.copy_(fc1_W_tensor)
+#     model.fc1.bias.copy_(fc1_b_tensor)
+#     model.fc2.weight.copy_(fc2_W_tensor)
+#     model.fc2.bias.copy_(fc2_b_tensor)
+#     model.fc3.weight.copy_(fc3_W_tensor)
+#     model.fc3.bias.copy_(fc3_b_tensor)
+
+# X = torch.ones(1,4)
+
+# logits = model(X)
+# print(logits)
 
 
