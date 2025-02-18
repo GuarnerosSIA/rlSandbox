d3d0c37c66c69991c6719dd7323e9dfa2d176373
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-02-18 16:33:06 -0600

    The platform in coppelia can sense the information in coppelia and send information

--- stewartPlatformCoppelia.py
+++ stewartPlatformCoppelia.py
@@ -7,14 +7,25 @@ import json
 time.sleep(1)
 
 
-client = RemoteAPIClient(port=23000)
+client = RemoteAPIClient(port=23001)
 sim = client.require('sim')
 sim.setStepping(True)
 sim.startSimulation()
 
-    
+joint_handles = [
+    sim.getObject('/stewartPlatform/motor1'),
+    sim.getObject('/stewartPlatform/motor2'),
+    sim.getObject('/stewartPlatform/motor3'),
+    sim.getObject('/stewartPlatform/motor4'),
+    sim.getObject('/stewartPlatform/motor5'),
+    sim.getObject('/stewartPlatform/motor6')
+]
+
+
 while (t := sim.getSimulationTime()) < 20:
     print(f'Simulation time: {t:.2f} [s]')
+    joint_positions = [sim.getJointPosition(joint) for joint in joint_handles]
+    print(f'Joint positions: {joint_positions}')
     value = int(np.sin(t)*500+500)
     sim.setStringSignal("data_1", str(value))
     value = int(np.cos(t)*500+500)
@@ -27,6 +38,5 @@ while (t := sim.getSimulationTime()) < 20:
     sim.setStringSignal("data_5", str(value))
     value = int(np.cos(t)*500+500)
     sim.setStringSignal("data_6", str(value))
-
     sim.step()
 sim.stopSimulation()
