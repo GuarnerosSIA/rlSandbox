348cd7a9bef90ef44cf74d37d7fc51c0e441e7a2
GuarnerosSIA <alejandrowarnsan@gmail.com>
2025-02-18 12:55:47 -0600

    The Stewart platform in coppelia follows the intrsuctions from python

--- stewartPlatformCoppelia.py
+++ stewartPlatformCoppelia.py
@@ -7,7 +7,7 @@ import json
 time.sleep(1)
 
 
-client = RemoteAPIClient(port=23001)
+client = RemoteAPIClient(port=23000)
 sim = client.require('sim')
 sim.setStepping(True)
 sim.startSimulation()
@@ -15,12 +15,18 @@ sim.startSimulation()
     
 while (t := sim.getSimulationTime()) < 20:
     print(f'Simulation time: {t:.2f} [s]')
-    data_to_send = {
-        "joint_position":[0.1,0.2,0.3,0.4,0.5,0.6],
-        "messager": "Update joint positions"
-    }
-    data_string = json.dumps(data_to_send)
-    sim.setStringSignal("data_from_python", data_string)
- 
+    value = int(np.sin(t)*500+500)
+    sim.setStringSignal("data_1", str(value))
+    value = int(np.cos(t)*500+500)
+    sim.setStringSignal("data_2", str(value))
+    value = int(np.sin(t)*500+500)
+    sim.setStringSignal("data_3", str(value))
+    value = int(np.cos(t)*500+500)
+    sim.setStringSignal("data_4", str(value))
+    value = int(np.sin(t)*500+500)
+    sim.setStringSignal("data_5", str(value))
+    value = int(np.cos(t)*500+500)
+    sim.setStringSignal("data_6", str(value))
+
     sim.step()
 sim.stopSimulation()
